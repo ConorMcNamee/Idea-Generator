c50230dfc4b585fdeececbdae49584f09f1a7433
40298979 <cmcnamee25@qub.ac.uk>
2023-03-10 15:07:40 +0000

    Now Scraps Each link for a given link and pulls h1, h3 and li elements from a markdown file

--- idea_gen/spiders/github_spider.py
+++ idea_gen/spiders/github_spider.py
@@ -9,18 +9,25 @@ class GithubSpider(scrapy.Spider):
     def start_requests(self):
         # go through each search query for ideas
         urls = [
-            'https://github.com/search?',
+            'https://github.com/search?q={}'.format("project+ideas"),
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         #get all links from search results
-        # response.css('.repo-list-item .v-align-middle::attr(href)').getall()
-
-        
-        
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        Path(filename).write_bytes(response.body)
-        self.log(f'Saved file {filename}')
+        # 
+        for link in response.css('.repo-list-item .v-align-middle::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_repo)
+    
+    def parse_repo(self, response):
+        markdown = response.css('.markdown-body')
+        for element in markdown:
+            yield {
+                'h1': element.css('h1::text').get().strip(),
+                'h3': element.css('h2::text').getall(),
+            }
+            for li in element.css('li::text').getall():
+                yield {
+                    li.strip()
+                }
